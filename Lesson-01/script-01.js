282b8c9c19f5b167978c59a567b0e1031f1780f6
verm-v <verm-v@yandex.ru>
2021-06-23 13:13:42 +0600

    Задание 1-го урока выполнено

---
+++ Lesson-01/script-01.js
@@ -0,0 +1,15 @@
+﻿function Script_01(){
+    var Tc = prompt("Введите значение температуры в градусах Цельсия: ", "0");
+    if(!isNaN(Tc)){
+        Tc = parseFloat(Tc);
+        var Tf = (9/5) * Tc + 32;
+        alert ("Значение температуры по Фаренгейту: " + Tf);
+
+        //Вывод результата на html-страницу
+        document.getElementById("t01_01").innerHTML = ("Значение температуры по Цельсию: " + Tc);
+        document.getElementById("t01_02").innerHTML = ("Значение температуры по Фаренгейту: " + Tf);
+    }else{
+        alert ("Введены неверные данные!")
+    }
+
+}
