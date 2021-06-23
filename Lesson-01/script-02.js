86b7d7e2c25f6b7a556f8bb8a5b5d4b477d79a14
verm-v <verm-v@yandex.ru>
2021-06-23 13:21:05 +0600

    Убран лишний var во 2-м скрипте

--- Lesson-01/script-02.js
+++ Lesson-01/script-02.js
@@ -1,6 +1,6 @@
 ﻿var admin;
 var name = "Василий";
-var admin = name;
+admin = name;
 
 //Вывод результата на html-страницу
 document.getElementById("t02_01").innerHTML = ("Переменная <b>name</b> содержит значение: \"" + name + "\"");
