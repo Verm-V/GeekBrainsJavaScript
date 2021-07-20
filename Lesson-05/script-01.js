197cee1ef885ba3ffa3235eaede94b705ec6bfee
Verm-V <Verm-V@yandex.ru>
2021-07-20 22:51:57 +0600

    Задание 5-го урока полностью выполнено

--- Lesson-05/script-01.js
+++ Lesson-05/script-01.js
@@ -1,8 +1,9 @@
-﻿//Рисование шахматной доски
-function drawBoard() {
-    let board = document.getElementById("board");
-    let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];//Список букв для подписей клеток
-    clearItem(board);
+﻿let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];//Список букв для подписей клеток
+
+//Рисование шахматной доски
+function DrawBoard() {
+    let board = document.getElementById("board");//Находим элемент в котором будем создавать шахматную доску
+    clearItem(board);//Очищаем ее
 
     var table = document.createElement("table");
     table.className = "chess-board";
@@ -52,9 +53,78 @@ function drawBoard() {
     console.log("test");
 }
 
+//Расставляет на доске фигуры в виде букв (вызывается из html-документа)
 function FiguresArrangementLetters() {
-    
+    FiguresArrangement(false);
 }
+
+//Расставляет на доске фигуры в виде картинок (вызывается из html-документа)
+function FiguresArrangementPictures() {
+    FiguresArrangement(true);
+}
+
+//Расстановка фигур на доске
+//isFigurePicture: true - фигуры картинками, false - фигуры текстом
+function FiguresArrangement(isFigurePicture) {
+    DrawBoard();//Рисует доску (на тот случай если она еще не нарисована)
+    let cellLabel = "";
+
+    //Расстановка пешек
+    for (let i = 0; i < 8; i++){
+        cellLabel = letters[i] + "2";
+        ArrangeSingleFigure(cellLabel, "P", true, isFigurePicture);
+        cellLabel = letters[i] + "7";
+        ArrangeSingleFigure(cellLabel, "P", false, isFigurePicture);
+    }
+
+    //Расстановка остальных фигур (сначала белые, потом черные)
+    let color = true;
+    let row = 1;
+    do {
+        cellLabel = "A" + row;
+        ArrangeSingleFigure(cellLabel, "R", color, isFigurePicture);
+        cellLabel = "B" + row;
+        ArrangeSingleFigure(cellLabel, "N", color, isFigurePicture);
+        cellLabel = "C" + row;
+        ArrangeSingleFigure(cellLabel, "B", color, isFigurePicture);
+        cellLabel = "D" + row;
+        ArrangeSingleFigure(cellLabel, "Q", color, isFigurePicture);
+        cellLabel = "E" + row;
+        ArrangeSingleFigure(cellLabel, "K", color, isFigurePicture);
+        cellLabel = "F" + row;
+        ArrangeSingleFigure(cellLabel, "B", color, isFigurePicture);
+        cellLabel = "G" + row;
+        ArrangeSingleFigure(cellLabel, "N", color, isFigurePicture);
+        cellLabel = "H" + row;
+        ArrangeSingleFigure(cellLabel, "R", color, isFigurePicture);
+
+        color = !color;
+        row = 8;
+    } while (!color)
+
+
+}
+
+//Устанавливает фигуру (текстовым обозначением) на доске
+//cellLabel: координаты клетки (например E2)
+//figure: буквенное обозначение фигуры
+//isFigureWhite: true - фигура белая, false - фигура черная
+function ArrangeSingleFigure(cellLabel, figure, isFigureWhite, isFigurePicture) {
+    let cell = document.getElementById(cellLabel);
+    if (isFigurePicture) {
+        let picture = document.createElement('img');
+        let fileName = "./pics/" + figure + (isFigureWhite ? "White" : "Black") + ".png";
+        picture.src = fileName;
+        cell.appendChild(picture);    
+    } else {
+        let text = document.createElement('span');
+        text.innerHTML = figure;
+        text.className = isFigureWhite ? "white-figure" : "black-figure";
+        cell.appendChild(text);    
+    }
+
+}
+
 
 //Очищает DOM элемент от всех его дочерних элементов.
 function clearItem(item) {
